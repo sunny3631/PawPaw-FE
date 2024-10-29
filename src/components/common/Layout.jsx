f2dfd1331b88aad1fb15ba6cfc8f8500edcb1144
indianaPoly <hyunrim03@naver.com>
2024-10-29 13:51:13 +0900

    design: layout

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -2,16 +2,39 @@ import { useState } from "react";
 
 import Header from "./Header";
 import Navigation from "./Navigation";
+import styled from "styled-components";
 
-const Layout = ({ name, imgUrl, children }) => {
+const Layout = ({ name, age, imgUrl, children }) => {
   const [activate, setActivate] = useState();
   return (
-    <div>
-      <Header name={name} imgUrl={imgUrl} />
-      {children}
-      <Navigation activate={activate} setActivate={setActivate} />
-    </div>
+    <LayoutContainer>
+      <Header name={name} age={age} imgUrl={imgUrl} />
+      <Content>{children}</Content>
+      <NavigationWrapper>
+        <Navigation activate={activate} setActivate={setActivate} />
+      </NavigationWrapper>
+    </LayoutContainer>
   );
 };
+
+const LayoutContainer = styled.div`
+  background-color: #ffeccf;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Content = styled.div`
+  flex: 1; /* children의 내용을 화면의 대부분을 차지하게 만듭니다 */
+  overflow-y: auto; /* 내용이 많을 경우 스크롤이 가능하도록 설정 */
+  padding-bottom: 60px; /* 하단의 Navigation 공간을 확보하기 위해 여백 추가 */
+`;
+
+const NavigationWrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background-color: #fff;
+`;
 
 export default Layout;
