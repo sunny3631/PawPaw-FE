ef2653736808ee083856e3333a59f4bb531d2262
indianaPoly <hyunrim03@naver.com>
2024-10-29 13:51:20 +0900

    design: header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import wave from "../../assets/background/wave.svg";
 
-const Header = ({ name, imgUrl }) => {
+const Header = ({ name, age, imgUrl }) => {
   return (
     <HeaderContainer>
       <Title>PAWPAW</Title>
@@ -8,7 +9,10 @@ const Header = ({ name, imgUrl }) => {
         <RepresentImageWrapper>
           <RepresentImage src={imgUrl} alt="" />
         </RepresentImageWrapper>
-        <Name>{name}</Name>
+        <InformationContainer>
+          <Name>{name}</Name>
+          <Age>{age}</Age>
+        </InformationContainer>
       </ProfileContainer>
     </HeaderContainer>
   );
@@ -18,31 +22,33 @@ const HeaderContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 111px;
-  background-color: #7fabd4;
-  position: relative;
+  height: 170px;
+  background-color: #f9d49b;
+  justify-content: start;
   padding: 20px; /* 패딩으로 내부 여백 설정 */
+  gap: 10px;
+  position: relative;
 `;
 
 const Title = styled.span`
-  margin-top: 25px;
-  font-size: 24px;
-  font-weight: bold;
-  color: black;
+  font-family: "KOTRAHOPE", sans-serif;
+  font-size: 32px;
+  font-weight: 400;
+  line-height: 37.28px;
+  text-align: left;
+  color: #4f2304;
 `;
 
 const ProfileContainer = styled.div`
   display: flex;
   align-items: center;
-  position: absolute;
-  bottom: -50px; /* 이미지가 컨테이너 아래쪽에 걸치게 설정 */
+  gap: 10px;
 `;
 
 const RepresentImageWrapper = styled.div`
-  width: 100px;
-  height: 100px;
+  width: 90px;
+  height: 90px;
   border-radius: 50%; /* 원형으로 만들기 */
-  border: 8px solid #89abda; /* 컨테이너와 동일한 색상의 테두리 */
   overflow: hidden; /* 이미지가 원형을 벗어나지 않게 자르기 */
   display: flex;
   justify-content: center;
@@ -55,12 +61,29 @@ const RepresentImage = styled.img`
   object-fit: cover; /* 이미지가 잘 맞도록 설정 */
 `;
 
+const InformationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 10px;
+`;
+
 const Name = styled.span`
-  font-size: 20px;
-  font-weight: bold;
-  color: white;
-  margin-left: 10px; /* 이름과 이미지 간의 간격 조정 */
-  margin-bottom: 20px;
+  font-family: Karla;
+  font-size: 25px;
+  font-weight: 700;
+  line-height: 29.23px;
+  letter-spacing: 0.08em;
+  text-align: left;
+  color: #6a5555;
+`;
+
+const Age = styled.span`
+  font-family: Karla;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 18.7px;
+  text-align: left;
+  color: #fffdde;
 `;
 
 export default Header;
