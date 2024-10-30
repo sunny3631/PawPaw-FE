4898bec75cf1127f476d77c40b2ad8cbded33682
indianaPoly <hyunrim03@naver.com>
2024-10-30 20:09:37 +0900

    design: mainpage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,47 @@
 import React from "react";
-import Layout from "../components/common/Layout.jsx";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import main from "../assets/image/main.png";
 
-export default function MainPage() {
-  return <Layout>처음 보이는 페이지 입니다.</Layout>;
-}
+const MainPage = () => {
+  const navigate = useNavigate();
+  setTimeout(() => {
+    navigate("/login");
+  }, 2000);
+
+  return (
+    <Container>
+      <Image src={main} alt="" />
+      <Title>PAWPAW</Title>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #ffcc80;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative; // 이미지 위에 텍스트 배치를 위한 설정
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
+const Title = styled.span`
+  position: absolute; // 이미지 위에 텍스트 위치 설정
+  font-family: KOTRA HOPE;
+  font-size: 32px;
+  font-weight: 900;
+  color: #4f2304;
+  text-align: center;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%); // 텍스트를 이미지 중앙에 배치
+`;
+
+export default MainPage;
