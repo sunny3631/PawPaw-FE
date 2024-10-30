1bc0102869dfdea1c3445bc6396b248950a16308
indianaPoly <hyunrim03@naver.com>
2024-10-30 20:09:23 +0900

    design: login

--- src/components/login/ConnectWallet.jsx
+++ src/components/login/ConnectWallet.jsx
@@ -1,11 +1,38 @@
-const ConnectWallet = ({ walletAddress, handleFunc }) => {
+import styled from "styled-components";
+import metamaskLogo from "../../assets/icons/Metamask.svg";
+
+const ConnectWallet = ({ handleFunc }) => {
   return (
-    <div>
-      <button onClick={handleFunc}>
-        {walletAddress ? "지갑 연결 됨" : "지갑 연결 해야됨"}
-      </button>
-    </div>
+    <Container onClick={handleFunc}>
+      <MetaMaskImg src={metamaskLogo} alt="메타마스크" />
+      <Title>METAMASK</Title>
+    </Container>
   );
 };
+
+const Container = styled.button`
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  align-items: center;
+  width: 241px;
+  height: 52px;
+  border-radius: 7px;
+  border: 1.5px solid #ffeccf;
+  background-color: rgba(255, 255, 255, 0.1); /* 배경만 투명도 적용 */
+`;
+
+const MetaMaskImg = styled.img`
+  width: 24px;
+  height: 24px;
+`;
+
+const Title = styled.span`
+  font-family: Inter;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 24px;
+  text-align: left;
+`;
 
 export default ConnectWallet;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,49 @@
+import styled from "styled-components";
+import ConnectWallet from "../components/login/ConnectWallet";
+import useWallet from "../hooks/login/useWallet";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
-  return <div>로그인 페이지 입니다.</div>;
+  const { connectWallet, walletAddress } = useWallet();
+  const navigate = useNavigate();
+
+  const onClickWallet = async () => {
+    const isConnected = await connectWallet();
+    if (isConnected) {
+      console.log("성장적으로 연결되었습니다.");
+      console.log("사용자 메타마스크 주소 : ", walletAddress);
+      navigate("/select");
+    } else {
+      alert("메타마스크와 연결이 되지 않습니다.");
+    }
+  };
+
+  return (
+    <Container>
+      <Title>LOGIN</Title>
+      <ConnectWallet handleFunc={onClickWallet} />
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  background-color: #ffcc80;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  gap: 50px;
+`;
+
+const Title = styled.span`
+  font-family: KOTRA HOPE;
+  font-size: 64px;
+  font-weight: 900;
+  line-height: 74.56px;
+  text-align: left;
+  color: #4f2304;
+`;
 
 export default Login;
